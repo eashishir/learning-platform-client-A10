519965ae8c879bd194ac785f08931ec761f0679a
eashishir <eftekharahmedshishir@gmail.com>
2022-10-27 10:33:59 -0700

    faq another add

--- src/Pages/FAQ/FaqPage.js
+++ src/Pages/FAQ/FaqPage.js
@@ -17,6 +17,12 @@ const FaqPage = () => {
                         Visualize your knowledge map.
                     </Accordion.Body>
                 </Accordion.Item>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>#1.How many videos in this course?</Accordion.Header>
+                    <Accordion.Body>
+                       There are 250+ course in this courses.You con save this video to your mobile.And you will watch this video offline.
+                    </Accordion.Body>
+                </Accordion.Item>
                
             </Accordion>
         </div>
